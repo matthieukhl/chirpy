ceafc6cfa9afc75b6ead6819b9f08a05be91f2bc
Matthieu Khairallah <matthieu.khairallah@gmail.com>
2026-02-23 15:30:19 +0100

    updated GET /api/chirps to retrieve chirps from a specific user ID if given an 'author_id' optinal query parameter, if not, endpoint returns all chirps as before

--- handlers/getChirps.go
+++ handlers/getChirps.go
@@ -1,23 +1,60 @@
 package handlers
 
 import (
+	"database/sql"
 	"encoding/json"
 	"net/http"
+
+	"github.com/google/uuid"
 )
 
 func (a *ApiConfig) GetChirps(w http.ResponseWriter, r *http.Request) {
-	chirps, err := a.Queries.GetAllChirps(r.Context())
+	// Retrieve optinal author_id parameter
+	author_id := r.URL.Query().Get("author_id")
+
+	if author_id == "" {
+		chirps, err := a.Queries.GetAllChirps(r.Context())
+		if err != nil {
+			a.Logger.Error(err.Error(), "endpoint", "GET /api/chirps")
+			respondWithError(w, http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		if err := json.NewEncoder(w).Encode(chirps); err != nil {
+			a.Logger.Error(err.Error(), "endpoint", "GET /api/chirps")
+			respondWithError(w, http.StatusInternalServerError)
+			return
+		}
+	}
+
+	authorUUID, err := uuid.Parse(author_id)
 	if err != nil {
-		a.Logger.Error(err.Error(), "endpoint", "GET /api/chirps")
-		respondWithError(w, http.StatusInternalServerError)
+		a.Logger.Error(err.Error(), "endpoint", r.URL.String())
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(map[string]string{"error": "bad request"})
 		return
 	}
 
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	if err := json.NewEncoder(w).Encode(chirps); err != nil {
-		a.Logger.Error(err.Error(), "endpoint", "GET /api/chirps")
-		respondWithError(w, http.StatusInternalServerError)
+	chirps, err := a.Queries.GetChirpsByUserId(r.Context(), authorUUID)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			a.Logger.Error(err.Error(), "endpoint", r.URL.String())
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusNotFound)
+			json.NewEncoder(w).Encode(map[string]string{"error": "user id not found"})
+			return
+		}
+		a.Logger.Error(err.Error(), "endpoint", r.URL.String())
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(map[string]string{"error": "internal server error"})
 		return
 	}
+
+	w.Header().Set("Content-Type", "appllication/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(chirps)
 }
